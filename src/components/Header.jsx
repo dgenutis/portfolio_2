1e3ec9c1a4f86b94b72e9d93dd14cf8f6d2cf54c
dgenutis <dainiusgenutis@yahoo.com>
2024-09-20 11:01:24 +0300

    header corection

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,31 @@
+import React, { useEffect, useState } from "react";
 import "./Header.css";
 import { Link } from "react-scroll";
 
 const Header = () => {
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [hidden, setHidden] = useState(false);
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+    if (currentScrollY > lastScrollY && currentScrollY > 90) {
+      setHidden(true);
+    } else {
+      setHidden(false);
+    }
+    setLastScrollY(currentScrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
     <>
-      <header>
+      <header className={hidden ? "hidden" : ""}>
         <a href="#" className="logo">
           Dainius Genutis
         </a>
@@ -13,7 +34,7 @@ const Header = () => {
             About Me
           </Link>
           <Link to="container4" smooth={true} duration={500}>
-          Projects
+            Projects
           </Link>
           <a href="#">Say Hello</a>
         </nav>
