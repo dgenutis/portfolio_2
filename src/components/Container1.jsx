4e26ba03e7ed3ab7ed235f0441046f56dffcdd7e
dgenutis <dainiusgenutis@yahoo.com>
2024-09-21 11:39:15 +0300

    arrow up

--- src/components/Container1.jsx
+++ src/components/Container1.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import "./Container1.css";
+import { Link } from "react-scroll";
 
 const Container1 = () => {
   const containerRef = useRef(null);
@@ -28,16 +29,29 @@ const Container1 = () => {
 
   return (
     <div className="container">
-    <div className="container-1" ref={containerRef}>
-      <div className="title">
-        <h2>Junior JavaScript</h2>
-        <h1>Full-Stack Developer</h1>
+      <div className="container-1" ref={containerRef}>
+        <div className="title">
+          <h2>Junior JavaScript</h2>
+          <h1>Full-Stack Developer</h1>
+        </div>
+        <div className="photo-container">
+          <img src="/2.jpg" alt="Developer" className="photo" />
+        </div>
       </div>
-      <div className="photo-container">
-        <img src="/2.jpg" alt="Developer" className="photo" />
-      </div>
-    </div>
-
+      <Link to="container" smooth={true} duration={500}>
+        <svg
+          stroke="currentColor"
+          fill="currentColor"
+          stroke-width="0"
+          version="1.1"
+          viewBox="0 0 16 16"
+          height="3em"
+          width="3em"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M8 0.5l-7.5 7.5h4.5v8h6v-8h4.5z"></path>
+        </svg>
+      </Link>
     </div>
   );
 };
--- src/components/Container4.jsx
+++ src/components/Container4.jsx
@@ -137,7 +137,18 @@ const Container4 = () => {
       </div>
       <div className="container4-2">
         <a href="#" ref={linkRef}>
-          view all <i className="fa fa-th" aria-hidden="true"></i>
+          view all{" "}
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            stroke-width="0"
+            viewBox="0 0 1024 1024"
+            height="1.5em"
+            width="1.5em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M523.8 191.4v288.9h382V128.1zm0 642.2l382 62.2v-352h-382zM120.1 480.2H443V201.9l-322.9 53.5zm0 290.4L443 823.2V543.8H120.1z"></path>
+          </svg>
         </a>
       </div>
     </>
