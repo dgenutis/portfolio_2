c91e6b48c7cdaada1d3a54ec6f64f3ec5319eb13
dgenutis <dainiusgenutis@yahoo.com>
2024-09-25 21:11:28 +0300

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Container2 from "./components/Container2";
 import Container3 from "./components/Container3";
 import Container4 from "./components/Container4";
 import Container5 from "./components/Container5";
+import Container6 from "./components/Container6";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
@@ -16,6 +17,7 @@ function App() {
       <Container3 />
       <Container4 />
       <Container5 />
+      <Container6 />
       <Footer />
     </>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,17 @@ import './Footer.css'
 const Footer = () => {
   return (
     <>
-    <div className="footer">
-<p>
- &copy; 2024 | Dainius Genutis
- 
-</p>
-    </div>
+
+    <div className="footer0">
+      <div className="footer">
   
+        <p>
+          &copy; 2024 | Dainius Genutis
+
+        </p>
+      </div>
+
+    </div>
     </>
   );
 };
