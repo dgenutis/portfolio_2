0c1d7c44fd9ae3251a5d889403f38c3970bb0424
dgenutis <dainiusgenutis@yahoo.com>
2024-09-25 21:16:59 +0300

    delete Container6.jsx nereikalingas

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Container2 from "./components/Container2";
 import Container3 from "./components/Container3";
 import Container4 from "./components/Container4";
 import Container5 from "./components/Container5";
-import Container6 from "./components/Container6";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
@@ -17,7 +16,6 @@ function App() {
       <Container3 />
       <Container4 />
       <Container5 />
-      <Container6 />
       <Footer />
     </>
   );
